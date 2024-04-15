67875fe54c3ef0f6598ad763f5a944c6e93a10d8
Alexandra <alexandradias05@ua.pt>
2024-04-15 16:37:03 +0100

    Update TP1.java

--- TP1.java
+++ TP1.java
@@ -1,78 +1,87 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
 
 public class TP1 {
     public static void main(String[] args) {
 
-        // Criação um objeto File para o diretório "Distritos" e obtém uma lista de todos os ficheiros nesse diretório.
-
         File folder = new File("Distritos");
         File[] listOfFiles = folder.listFiles();
 
-        // Criação um HashMap chamado totalVotos para armazenar o total de votos de cada partido em todos os círculos eleitorais.
-
         Map<String, Integer> totalVotos = new HashMap<>();
-        
+
         for (File file : listOfFiles) {
-            if (file.isFile()) { // Verifica se o ficheiro é realmente um ficheiro (e não um diretório).
+            if (file.isFile()) {
                 try {
                     ObjectInputStream iS = new ObjectInputStream(new FileInputStream(file));
-                    VotosCirculoEleitoral leitura = (VotosCirculoEleitoral) iS.readObject(); // Lê um objeto do ficheiro e faz cast para VotosCirculoEleitoral.
-        
-                    System.out.println("Distrito: " + leitura.getNomeCirculo()); // Imprime o nome do círculo eleitoral.
-        
-                    for (String conselho : leitura.getVotosPorConcelho().keySet()) { // Para cada concelho no círculo eleitoral, imprime o nome do concelho e os votos de cada partido nesse concelho.
+                    VotosCirculoEleitoral leitura = (VotosCirculoEleitoral) iS.readObject();
+
+                    System.out.println("Distrito: " + leitura.getNomeCirculo());
+
+                    for (String conselho : leitura.getVotosPorConcelho().keySet()) {
+
                         System.out.println("  Concelho: " + conselho);
                         VotosConcelho votosPorPartido = leitura.getVotosPorConcelho().get(conselho);
                         System.out.println("  Partidos do " + conselho);
                         Map<String, Integer> votos = votosPorPartido.getVotosPorPartido();
-                        for (Map.Entry<String, Integer> entry : votos.entrySet()) { 
+                        for (Map.Entry<String, Integer> entry : votos.entrySet()) {
                             System.out.println("    Partido: " + entry.getKey() + ", Votos: " + entry.getValue());
                         }
                     }
-                    
-                    for (String conselho : leitura.getVotosPorConcelho().keySet()) { // Para cada concelho no círculo eleitoral, adiciona os votos de cada partido ao total de votos.
+
+                    for (String conselho : leitura.getVotosPorConcelho().keySet()) {
                         VotosConcelho votosPorPartido = leitura.getVotosPorConcelho().get(conselho);
                         Map<String, Integer> votos = votosPorPartido.getVotosPorPartido();
                         for (Map.Entry<String, Integer> entry : votos.entrySet()) {
-                            totalVotos.put(entry.getKey(), totalVotos.getOrDefault(entry.getKey(), 0) + entry.getValue());
+                            totalVotos.put(entry.getKey(),
+                                    totalVotos.getOrDefault(entry.getKey(), 0) + entry.getValue());
                         }
                     }
-
-                    iS.close(); // Fecha o ObjectInputStream.
+                    iS.close();
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                     System.out.println("Erro!");
                 }
             }
-        }
-        System.out.println("--------------------Total Nacional-------------------");
-        // Imprime o total de votos de cada partido em todos os círculos eleitorais.
-        int totalGeralVotos = 0;
-        for (Integer votos : totalVotos.values()) {
-            totalGeralVotos += votos;
-        }
-        System.out.println("Total de votos: " + totalGeralVotos);
-        
-        try {
-            PrintWriter writer = new PrintWriter("Resultado/TotalNacional.txt", "UTF-8");
-            writer.println("Total de votos: " + totalGeralVotos);
-            
-            for (Map.Entry<String, Integer> entry : totalVotos.entrySet()) {
-                double percentagem = (double) entry.getValue() / totalGeralVotos * 100;
-                System.out.printf("Partido: %s, Votos: %d, Percentagem: %.2f%%\n", entry.getKey(), entry.getValue(), percentagem);
-                writer.printf("Partido: %s, Votos: %d, Percentagem: %.2f%%\n", entry.getKey(), entry.getValue(), percentagem);
+
+            int totalGeralVotos = 0;
+            int votosBrancos = totalVotos.getOrDefault("Brancos", 0);
+            int votosNulos = totalVotos.getOrDefault("Nulos", 0);
+            for (Integer votos : totalVotos.values()) {
+                totalGeralVotos += votos;
             }
+            int totalVotantes = totalGeralVotos + votosBrancos + votosNulos;
+            DecimalFormat df = new DecimalFormat("#.##");
+
+            totalVotos.remove("Brancos");
+            totalVotos.remove("Nulos");
+
+            List<Map.Entry<String, Integer>> list = new ArrayList<>(totalVotos.entrySet());
+            list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+            try (PrintWriter writer = new PrintWriter("Resultado/TotalNacional.txt", "UTF-8")) {
+                writer.println("--------------------Total Nacional-------------------");
+                writer.printf("Total de votantes: %d\n", totalVotantes);
+                writer.printf("Total de votos validos: %d Percentagem: %s%%\n", totalGeralVotos,
+                        df.format((float) totalGeralVotos / totalVotantes * 100));
+                writer.printf("Votos Brancos: %d Percentagem: %s%%\n", votosBrancos,
+                        df.format((float) votosBrancos / totalVotantes * 100));
+                writer.printf("Votos Nulos: %d Percentagem: %s%%\n", votosNulos,
+                        df.format((float) votosNulos / totalVotantes * 100));
+                writer.println("--------------------Votos por Partido-------------------");
+
+                for (Map.Entry<String, Integer> entry : list) {
+                    double percentagem = (double) entry.getValue() / totalVotantes * 100;
+                    writer.printf("Partido: %s, Votos: %d, Percentagem: %.2f%%\n", entry.getKey(), entry.getValue(),
+                            percentagem);
+                }
             
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Erro!");
-            e.printStackTrace();
+            System.out.println("Total nacional no ficheiro TotalNacional.txt");
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Erro!");
+            }
         }
     }
 }
